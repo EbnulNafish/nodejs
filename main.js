356d224363649d3bd2607eba3ed8a21aa91f70d3
nafish <nafishebnulamirul96@gmail.com>
2022-05-19 13:06:05 +0600

    first node.js commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server=http.createServer(function(req,res){
+    res.end('Hello World.This is my first Node.js Programe');
+})
+
+server.listen(3000);
+console.log('Server Run success');
+
